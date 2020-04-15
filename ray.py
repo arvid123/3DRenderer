395b31f0e4506e8aee365a6c3ad48affd0696823
Arvid Lagerqvist <arvid.lagerqvist@live.se>
2020-04-15 16:30:13 +0200

    Add comment to Ray class
    The ray object can be somewhat hard to understand
    and it is very important to understand it mathematically to
    understand the raytracer, therefore comments are important here.

--- ray.py
+++ ray.py
@@ -1,5 +1,15 @@
 import util
 
+# A ray is similar to a line except that it doesn't extend behind the origin.
+# A ray is defined as the set of points described by these equations:
+#
+# x = origin.x + t * direction.x
+# y = origin.y + t * direction.y
+# z = origin.z + t * direction.z
+# t > 0
+# 
+# Where t is an independent parameter.
+
 class Ray:
     def __init__(self, origin, direction):
         self.origin = origin
